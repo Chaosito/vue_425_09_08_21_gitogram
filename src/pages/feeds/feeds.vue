88159049f0d89412d6f2180a82eacb8dcd9683f9
Sergey Aleinikov <saleinikov@kazzinc.com>
2021-08-12 16:42:37 +0600

    list of components

--- src/pages/feeds/feeds.vue
+++ src/pages/feeds/feeds.vue
@@ -5,20 +5,42 @@
                 <div class="icon"><icon name="home" /></div>
             </template>
             <template #content>
-                <h2>Stories</h2>
+                <ul class="stories">
+                    <li class="stories-item" v-for="story in stories" :key="story.id">
+                        <story-user-item
+                            :avatar="story.avatar"
+                            :username="story.username"
+                            @onPress="handlePress(story.id)"
+                        />
+                    </li>
+                </ul>
             </template>
         </topline>
     </div>
 </template>
 
 <script>
+import StoryUserItem from '../../components/storyUserItem/storyUserItem.vue'
 import { topline } from '../../components/topline'
 import { icon } from '../../icons'
+import stories from './data.json'
+
 export default {
   name: 'feeds',
   components: {
     topline,
-    icon
+    icon,
+    StoryUserItem
+  },
+  data () {
+    return {
+      stories
+    }
+  },
+  methods: {
+    handlePress (val) {
+      console.log(val, stories.filter(story => story.id === val)[0])
+    }
   }
 }
 </script>
@@ -29,3 +51,4 @@ export default {
     width: 50px;
 }
 </style>
+<style lang="scss" scoped src="./feeds.scss"></style>
