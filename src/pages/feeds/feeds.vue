d3c713838fb6b237659729eb67481b2f78aee684
Sergey Aleinikov <saleinikov@kazzinc.com>
2021-08-12 15:38:28 +0600

    Header component

--- src/pages/feeds/feeds.vue
+++ src/pages/feeds/feeds.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Hello!</h1>
+    <div class="topline">
+        <topline>
+            <template #headline>
+                <h1>Header</h1>
+            </template>
+            <template #content>
+                <h2>Stories</h2>
+            </template>
+        </topline>
+    </div>
 </template>
+
+<script>
+import { topline } from '../../components/topline'
+export default {
+  name: 'feeds',
+  components: {
+    topline
+  }
+}
+</script>
