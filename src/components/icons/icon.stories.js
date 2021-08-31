151ed1c1e063defedf38f5b46bdcf36a4f28241e
Sergey Aleinikov <chaositomail@gmail.com>
2021-08-31 20:35:38 +0600

    Fixed icons component in SB

--- src/components/icons/icon.stories.js
+++ src/components/icons/icon.stories.js
@@ -1,3 +1,4 @@
+import * as icons from './variants'
 import icon from './icon.vue'
 
 export default {
@@ -8,6 +9,12 @@ export default {
   argTypes: {
     iconColor: {
       control: { type: 'color' }
+    },
+    iconType: {
+      options: Object.keys(icons),
+      control: {
+        type: 'select'
+      }
     }
   }
 }
@@ -19,121 +26,14 @@ export const forkView = (args) => ({
   data () {
     return { args }
   },
-  template: `<span class="icon" style=""><icon name="fork" style="width:25px; color: ${args.iconColor}" /></span>`
+  template: `<span class="icon" style=""><icon name="${args.iconType}" style="width:25px; color: ${args.iconColor}" /></span>`
 })
 
 forkView.args = {
-  iconColor: 'red'
+  iconColor: 'red',
+  iconType: 'home'
 }
 
 forkView.story = {
-  name: 'Fork'
-}
-
-export const homeView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="home" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-homeView.args = {
-  iconColor: 'black'
-}
-
-homeView.story = {
-  name: 'Home'
-}
-
-export const logoutView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="logout" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-logoutView.args = {
-  iconColor: 'green'
-}
-
-logoutView.story = {
-  name: 'Logout'
-}
-
-export const starView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="star" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-starView.args = {
-  iconColor: 'blue'
-}
-
-starView.story = {
-  name: 'Star'
-}
-
-export const triangleView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="triangle" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-triangleView.args = {
-  iconColor: 'pink'
-}
-
-triangleView.story = {
-  name: 'Triangle'
-}
-
-export const closeView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="close" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-closeView.args = {
-  iconColor: 'black'
-}
-
-closeView.story = {
-  name: 'Close'
-}
-
-export const spinView = (args) => ({
-  components: {
-    icon
-  },
-  data () {
-    return { args }
-  },
-  template: `<span class="icon" style=""><icon name="spin" style="width:25px; color: ${args.iconColor}" /></span>`
-})
-
-spinView.args = {
-  iconColor: 'black'
-}
-
-spinView.story = {
-  name: 'Spin'
+  name: 'Icons'
 }
