6df088b54abfeb466e61de385b7a7b0eef76c11d
Sergey Aleinikov <saleinikov@kazzinc.com>
2021-09-01 17:30:21 +0600

    Auth page

--- src/pages/auth/auth.vue
+++ src/pages/auth/auth.vue
@@ -1,3 +1,40 @@
 <template>
-    Auth....
+    <div class="page-auth">
+        <div class="page-content">
+            <div class="left-side">
+                <div class="logo">
+                    <logo />
+                </div>
+                <div class="descr">
+                    More than just one repository.
+                    This is our digital world.
+                </div>
+                <div class="gitbtn">
+                    <x-button><div class="button-text">Authorize with github <div class="icon"><icon name="github" /></div></div></x-button>
+                </div>
+            </div>
+            <div class="right-side">
+                <img src="../../assets/macbook.png" />
+            </div>
+        </div>
+        <div class="page-footer">
+            © Gitogram from Loftschool
+        </div>
+    </div>
 </template>
+
+<script>
+import logo from '../../components/logo'
+import button from '../../components/button'
+import icon from '../../components/icons'
+
+export default {
+  components: {
+    logo,
+    xButton: button,
+    icon
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./auth.scss"></style>
