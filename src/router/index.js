e70a40ea0cfd030a631df7d40706b085dc5cac76
Sergey Aleinikov <saleinikov@kazzinc.com>
2021-09-03 14:11:28 +0600

    Cheking auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+import * as api from '../api'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -7,7 +8,23 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  next()
+  const authRoute = to.name === 'auth'
+
+  try {
+    await api.user.getUser()
+    if (authRoute) {
+      next({ path: '/' })
+    }
+
+    next()
+  } catch (e) {
+    if (authRoute) {
+      next()
+      return
+    }
+
+    next({ name: 'auth' })
+  }
 })
 
 export default router
