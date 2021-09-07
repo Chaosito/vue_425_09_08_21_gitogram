29c053d289ac8688e2eaacba0640a2f4fb333485
Sergey Aleinikov <chaositomail@gmail.com>
2021-09-07 23:42:28 +0600

    Profile begin

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -1,7 +1,22 @@
 <template>
   <page-header />
-  <div class="x-container">
-    asd
+  <div class="x-container profile-screen">
+    <div class="left-side">
+      <h1>My profile</h1>
+      <div class="user-component">
+        <div class="userpic">
+          <img :src="user.avatar_url" />
+        </div>
+        <div class="userinfo">
+          <h2>{{ user.login }}</h2>
+          <div class="info">54 <span>reposts</span> 834 <a href="#">watchers</a></div>
+          <div class="username">{{ user.name }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="right-side">
+      <div v-for="n in 44" :key="n">Repositories</div>
+    </div>
   </div>
 </template>
 <script>
@@ -25,6 +40,9 @@ export default {
     ...mapActions({
       logout: 'auth/logout'
     })
+  },
+  async mounted () {
+    console.log(this.user)
   }
 }
 </script>
